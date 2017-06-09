57a12bc32a749a757b01c06d3ef5b0377429ac9c
Chris Woodall <christopherhwoodall@gmail.com>
2017-06-09 16:53:59 +0100

    added item class

---
+++ app/src/main/java/com/codeclan/example/shoppingbasketcode/Item.java
@@ -0,0 +1,18 @@
+package com.codeclan.example.shoppingbasketcode;
+
+/**
+ * Created by mac on 6/9/17.
+ */
+
+public class Item {
+
+    private int value;
+
+    public Item() {
+        this.value = 1;
+    }
+
+    public int checkValue() {
+        return value;
+    }
+}
---
+++ app/src/test/java/com/codeclan/example/shoppingbasketcode/ItemTest.java
@@ -0,0 +1,34 @@
+package com.codeclan.example.shoppingbasketcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mac on 6/9/17.
+ */
+public class ItemTest {
+
+    Item item;
+    Item item2;
+
+    @Before
+    public void before() {
+        item = new Item();
+        item2 = new Item();
+    }
+
+    @Test
+    public void itemExists() {
+        assertNotNull(item);
+    }
+
+    @Test
+    public void itemHasValue() {
+        assertEquals(1, item.checkValue());
+    }
+
+
+
+}
